9ed6aac9aa4f9a484aa375bd4f07f301c243833c
cpark999 <78054414+cpark999@users.noreply.github.com>
2024-04-06 21:48:46 -0400

    event type figures
    These scripts create a figure for a data type, which shows all the events for that type plotted according to a channel

---
+++ footalleventsfigure.m
@@ -0,0 +1,24 @@
+eventType = 771; 
+figure; 
+
+for i = 1:length(EEG.event)
+    if EEG.event(i).edftype == eventType
+        start = EEG.event(i).latency; 
+        if isfield(EEG.event, 'duration') && EEG.event(i).duration > 0
+            duration = EEG.event(i).duration;
+        else
+            duration = 313;
+        end
+        for ch = 1:25 
+            endIndex = min(size(EEG.data, 2), start + duration - 1);
+            eventSegment = EEG.data(ch, start:endIndex);
+            plot(eventSegment);
+            hold on; 
+        end
+    end
+end
+
+xlabel('Time (ms)');
+ylabel('Amplitude (uV)');
+title(sprintf('Event Type %d - All Occurrences', eventType));
+legend(arrayfun(@(ch) sprintf('Channel %d', ch), 1:25, 'UniformOutput', false));
